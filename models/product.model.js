86552faa59612a127792ca88d5fad957cd233bbe
jayson <sondanghuynh@gmail.com>
2024-01-31 23:50:38 +0700

    Products Page

--- models/product.model.js
+++ models/product.model.js
@@ -1,8 +1,21 @@
 const { default: mongoose } = require("mongoose");
 
-const productSchema = new mongoose.Schema({
-
-})
+const productSchema = new mongoose.Schema(
+    {
+        title: String,
+        description: String,
+        price: Number,
+        discountPercentage: Number,
+        rating: Number,
+        stock: Number,
+        brand: String,
+        category: String,
+        thumbnail: String,
+        status: String,
+        position: Number,
+        deleted: Boolean
+    }
+)
 
 const Product = mongoose.model("Product", productSchema, "products")
 
