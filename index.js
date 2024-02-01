9feb03fbd1d28eae0320889c5fd3b5a3f4356e06
jayson <sondanghuynh@gmail.com>
2024-02-01 21:19:37 +0700

    Layout Chung

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require('express')
 require('dotenv').config()
-
 const database = require('./config/database.js').connect()
 const app = express()
 const port = process.env.PORT
 const routeAdmin = require('./routes/admin/index.route')
 const route = require('./routes/client/index.route')
 
+//App Local Variables
+const systemConfig = require('./config/system.js')
+app.locals.prefixAdmin = systemConfig.prefixAdmin
+
+//Set Views Engine
 app.set('views', './views')
 app.set('view engine', 'pug')
 
