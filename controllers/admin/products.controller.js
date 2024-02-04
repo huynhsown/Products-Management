baed13d267cbf7424c2d7b26904198285e60d118
jayson <sondanghuynh@gmail.com>
2024-02-04 13:56:49 +0700

    Toi uu phan bo loc

--- controllers/admin/products.controller.js
+++ controllers/admin/products.controller.js
@@ -1,32 +1,9 @@
 const Product = require('../../models/product.model')
+const filterStatusHelper = require('../../helpers/filterStatus')
 // [GET] /admin/products 
 module.exports.products = async (req,res)=>{
-    let filterStatus = [
-        {
-            name: "Tất cả",
-            status: "",
-            class: ""
-        },
-        {
-            name: "Hoạt động",
-            status: "active",
-            class: ""
-        },
-        {
-            name: "Dừng hoạt động",
-            status: "inactive",
-            class: ""
-        }
-    ]
-
-    if(req.query.status){
-        const index = filterStatus.findIndex(item => item.status==req.query.status)
-        filterStatus[index].class = "active"
-    }
-    else{
-        filterStatus[0].class = "active"
-    }
-
+    //Filter
+    const filterStatus = filterStatusHelper(req.query)
     const find = {
         deleted: false
     }
