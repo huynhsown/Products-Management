df459406a00989be4ca65fb52a540a1bbe7f8ae4
jayson <sondanghuynh@gmail.com>
2024-02-04 14:47:13 +0700

    Toi uu phan tim kiem

--- controllers/admin/products.controller.js
+++ controllers/admin/products.controller.js
@@ -1,5 +1,6 @@
 const Product = require('../../models/product.model')
 const filterStatusHelper = require('../../helpers/filterStatus')
+const searchHelper = require('../../helpers/search')
 // [GET] /admin/products 
 module.exports.products = async (req,res)=>{
     //Filter
@@ -12,12 +13,9 @@ module.exports.products = async (req,res)=>{
         find.status = req.query.status
     }
 
-    let keyword = ""
-    if(req.query.keyword){
-        keyword = req.query.keyword
-        const regex = new RegExp(keyword, 'i')
-        find.title = regex
-    }
+    const objSearch = searchHelper(req.query)
+
+    if(objSearch.regex) find.title = objSearch.regex
 
     const products = await Product.find(find)
 
@@ -25,6 +23,6 @@ module.exports.products = async (req,res)=>{
         pageTitle: "Trang danh sach san pham",
         products: products,
         filterStatus: filterStatus,
-        keyword: keyword
+        keyword: objSearch.keyword
     });
 }
