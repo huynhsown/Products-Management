3cf96520ffd2609b047ff75c2e80bb3dde48a63d
jayson <sondanghuynh@gmail.com>
2024-02-20 00:01:54 +0700

    Tinh nang Preview Image Add New Product

--- public/admin/js/newProduct.js
+++ public/admin/js/newProduct.js
@@ -1,7 +1,16 @@
-const inputTitle = document.querySelector('input[name="title"]')
-const inputDesc = document.querySelector('textarea[name="description"]')
-const inputPrice = document.querySelector('input[name="price"]')
-const inputDiscount = document.querySelector('input[name="discountPercentage"]')
-const inputStock = document.querySelector('input[name="stock"]')
-const inputThumbnail = document.querySelector('input[name="thumbnail"]')
-const inputPosition = document.querySelector('input[name="position"]')
+const fileInput = document.getElementById('thumbnail');
+const previewImage = document.getElementById('preview-image');
+const closeThumbnail = document.getElementById('close-thumbnail')
+
+fileInput.addEventListener('change', event => {
+    if(event.target.files.length){
+        previewImage.src = URL.createObjectURL(event.target.files[0])
+        previewImage.setAttribute("class", "img-thumbnail d-flex")
+    }
+});
+
+closeThumbnail.addEventListener('click', ()=>{
+    fileInput.value = ""
+    previewImage.src = ""
+    previewImage.setAttribute("class", "img-thumbnail d-none")
+})
